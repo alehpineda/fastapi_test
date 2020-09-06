26c1f0239de3947ccead4f49cf2a6613fb5a40e4
Alejandro Pineda <ale.hpineda@gmail.com>
2020-09-05 19:53:39 -0500

    added health check test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hola": "Mundo"}
+
+
+def test_read_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"detail": "ok"}
